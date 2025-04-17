f7ff65643687d854fa210e6cdee2255087ce0464
Ludo DENIS <ludv.denis@gmail.com>
2025-04-17 15:59:21 +0200

    moifNavBar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -91,7 +91,7 @@ const styles = StyleSheet.create({
   navContainer: {
     flexDirection: 'row',      // Aligne les éléments horizontalement (de gauche à droite)
     backgroundColor: '#2D5E3D', 
-    height: 100,                // Hauteur fixe de la barre de navigation
+    height: 60,                // Hauteur fixe de la barre de navigation
     justifyContent: 'space-around', // Distribue l'espace également entre les éléments
     alignItems: 'center',      
     paddingHorizontal: 10,     
@@ -105,7 +105,7 @@ const styles = StyleSheet.create({
     flex: 1,                   // Chaque élément prend une part égale de l'espace disponible
     justifyContent: 'center',  
     alignItems: 'center',     
-    paddingVertical: 20,       
+    paddingVertical: 10,       
   },
   activeItem: {
     backgroundColor: '#1E4D2B', 
