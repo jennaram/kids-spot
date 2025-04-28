d27d473fbeee0e029b32370b01ca01fe1684db03
Jenna <jenna.ramia@gmail.com>
2025-04-28 15:40:02 +0200

    séparation des styles de la page main.tsx

--- app/main.tsx
+++ app/main.tsx
@@ -16,7 +16,9 @@ import { useNavigation } from '@react-navigation/native';
 import fetchNearbyPlaces from '@/api/fetchNearbyPlaces';
 import getUserLocation from '@/services/localisation';
 import { Navigation } from '@/components/NavBar/Navigation';
-import PlacePopUp from '@/components/MainMap/PlacePopUp'; // Import du nouveau composant
+import PlacePopUp from '@/components/MainMap/PlacePopUp'; 
+import styles from '@/app/style/MapScreen.style';
+
 
 // Icônes personnalisées
 const iconByType = {
@@ -188,83 +190,5 @@ export default function MapScreen() {
       <Navigation />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-  menuContainer: {
-    position: 'absolute',
-    top: Platform.OS === 'ios' ? 50 : 30,
-    left: 20,
-    zIndex: 100,
-  },
-  userMarker: {
-    width: 40,
-    height: 40,
-  },
-  cultureMarker: {
-    width: 40,
-    height: 40,
-    backgroundColor: '#ff9770',
-    borderRadius: 25,
-    elevation: 10,
-  },
-  foodMarker: {
-    width: 40,
-    height: 40,
-    backgroundColor: '#95d5b2',
-    borderRadius: 25,
-    elevation: 10,
-  },
-  loisirsMarker: {
-    width: 40,
-    height: 40,
-    backgroundColor: '#8ecae6',
-    borderRadius: 25,
-    elevation: 10,
-  },
-  switchButton: {
-    position: 'absolute',
-    bottom: 100,
-    right: 20,
-    backgroundColor: 'white',
-    borderRadius: 25,
-    width: 50,
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  },
-  switchIcon: {
-    width: 30,
-    height: 30,
-  },
-  errorContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-  },
-  errorText: {
-    color: 'red',
-    textAlign: 'center',
-    marginBottom: 20,
-  },
-  loadingContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
