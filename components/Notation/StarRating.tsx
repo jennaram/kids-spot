3bad68bce47a13f169cc1f73e94e5b538b8d1898
Jenna <jenna.ramia@gmail.com>
2025-05-02 15:36:39 +0200

    séparation style du component StarRating.tsx

--- components/Notation/StarRating.tsx
+++ components/Notation/StarRating.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-
+import styles from '@/app/style/StarRating.styles';
 interface StarRatingProps {
   rating: number;
   setRating: (rating: number) => void;
@@ -54,34 +54,6 @@ const StarRating: React.FC<StarRatingProps> = ({
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    marginBottom: 10,
-    width: '100%',
-  },
-  label: {
-    fontSize: 16,
-    marginBottom: 8,
-    fontWeight: '600',
-    textAlign: 'left', // Centrer le label
-  },
-  ratingContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center', // Conteneur principal centré
-    width: '100%',
-  },
-  ratingGroup: {
-    flexDirection: 'row',
-    justifyContent: 'center', // Groupe d'étoiles centré
-  },
-  star: {
-    fontSize: 30,
-    color: '#ddd',
-    marginHorizontal: 5, // Changé de marginRight à marginHorizontal pour un espacement égal
-  },
-  starSelected: {
-    color: '#f1c40f',
-  },
-});
+
 
 export default StarRating;
