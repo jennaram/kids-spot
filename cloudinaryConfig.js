be4dc26c7cf452bef06199155dbcf3e7c89df12f
Alexandre Fourquin <alexandrefourquin@hotmail.fr>
2025-05-12 14:52:57 +0200

    creation fichier configcloudinary.js , je ne merge pas car je bloque

---
+++ cloudinaryConfig.js
@@ -0,0 +1,9 @@
+import {Cloudinary} from 'cloudinary-react-native';
+
+const cloudinary = new Cloudinary({
+  cloud_name: 'KIDSPOTKEY',
+  api_key: '465798237744771',
+  api_secret: '*********************************' // À sécuriser côté backend si possible
+});
+
+export default cloudinary;git a
